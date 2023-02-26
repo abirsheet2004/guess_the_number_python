5a20da2ecda987a9e7ce9d560c3611e22a9aa550
Abir Sheet <117583880+AbirSheet2004@users.noreply.github.com>
2023-02-27 02:10:37 +0530

    callendar.py
    callendar code

---
+++ calendar.py
@@ -0,0 +1,11 @@
+import calendar
+yy=int (input("enter the yy for code   :  "))
+mm=int (input ("enter the mm for code   :  "))
+print (calendar.month(yy,mm))
+
+
+import calendar
+yr=int(input("enter the yr  :  "))
+mm=int (input("enter the mm  :  "))
+
+print (calendar.month(yr,mm))
